723190cabe3ab4bca9200d9d248b24aa45825a85
MaxJJ <mjefremov@gmail.com>
2016-08-03 19:05:29 +0300

    Product fields added

--- src/main/java/lv/javaguru/java2/domain/Product.java
+++ src/main/java/lv/javaguru/java2/domain/Product.java
@@ -1,4 +1,24 @@
 package lv.javaguru.java2.domain;
 
+import java.util.List;
+
 public class Product {
+
+    private Long productID;
+
+    private String vendorCode;
+    private String vendorName;
+    private String vendorDescription;
+
+    private String sellUnit;
+    private String buyUnit;
+
+    private Integer unitSellPrice;
+    private Integer unitBuyPrice;
+
+
+    private String displayName;
+    private String displayDescription;
+
+
 }
