90f5ea05f2a9624622b865a9b465715eb2480ffa
alex <mymail@somewhere.com>
2016-08-03 19:34:55 +0300

    some changes

---
+++ src/main/java/lv/javaguru/java2/domain/Customer.java
@@ -0,0 +1,32 @@
+package lv.javaguru.java2.domain;
+
+public class Customer implements User{
+
+    private long id;
+    private String fullName;
+    private String email;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getFullName() {
+        return fullName;
+    }
+
+    public void setFullName(String fullName) {
+        this.fullName = fullName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
