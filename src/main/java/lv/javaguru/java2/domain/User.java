ac936710b5ca724406d251ee858388cf1e344236
alex <mymail@somewhere.com>
2016-08-03 20:19:27 +0300

    some changes

--- src/main/java/lv/javaguru/java2/domain/User.java
+++ src/main/java/lv/javaguru/java2/domain/User.java
@@ -2,6 +2,15 @@ package lv.javaguru.java2.domain;
 
 public interface User {
 
+    long getId();
 
+    void setId(long id);
 
+    String getFullName();
+
+    void setFullName(String fullName) ;
+
+    String getEmail();
+
+    void setEmail(String email);
 }
