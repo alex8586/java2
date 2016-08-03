e227a4f0b92f444455c31c258b53e622c35915c8
MaxJJ <mjefremov@gmail.com>
2016-08-03 19:31:15 +0300

    Product fields added

--- src/main/java/lv/javaguru/java2/domain/Product.java
+++ src/main/java/lv/javaguru/java2/domain/Product.java
@@ -5,6 +5,7 @@ import java.util.List;
 public class Product {
 
     private Long productID;
+    private Long categoryID;
 
     private String vendorCode;
     private String vendorName;
@@ -20,5 +21,93 @@ public class Product {
     private String displayName;
     private String displayDescription;
 
+    private Float remainUnitQty;
 
+    // experimental
+//
+//    private List<ProductChanges> changes;
+//    private List<Suppliers> suppliers;
+
+
+
+
+    public Long getProductID() {
+        return productID;
+    }
+
+    public void setProductID(Long productID) {
+        this.productID = productID;
+    }
+
+    public String getVendorCode() {
+        return vendorCode;
+    }
+
+    public void setVendorCode(String vendorCode) {
+        this.vendorCode = vendorCode;
+    }
+
+    public String getVendorName() {
+        return vendorName;
+    }
+
+    public void setVendorName(String vendorName) {
+        this.vendorName = vendorName;
+    }
+
+    public String getVendorDescription() {
+        return vendorDescription;
+    }
+
+    public void setVendorDescription(String vendorDescription) {
+        this.vendorDescription = vendorDescription;
+    }
+
+    public String getSellUnit() {
+        return sellUnit;
+    }
+
+    public void setSellUnit(String sellUnit) {
+        this.sellUnit = sellUnit;
+    }
+
+    public String getBuyUnit() {
+        return buyUnit;
+    }
+
+    public void setBuyUnit(String buyUnit) {
+        this.buyUnit = buyUnit;
+    }
+
+    public Integer getUnitSellPrice() {
+        return unitSellPrice;
+    }
+
+    public void setUnitSellPrice(Integer unitSellPrice) {
+        this.unitSellPrice = unitSellPrice;
+    }
+
+    public Integer getUnitBuyPrice() {
+        return unitBuyPrice;
+    }
+
+    public void setUnitBuyPrice(Integer unitBuyPrice) {
+        this.unitBuyPrice = unitBuyPrice;
+    }
+
+    public String getDisplayName() {
+        return displayName;
+    }
+
+    public void setDisplayName(String displayName) {
+        this.displayName = displayName;
+    }
+
+    public String getDisplayDescription() {
+        return displayDescription;
+    }
+
+    public void setDisplayDescription(String displayDescription) {
+        this.displayDescription = displayDescription;
+    }
 }
