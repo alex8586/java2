5098c319acce742b73be464ae108db98ab5cca45
MaxJJ <mjefremov@gmail.com>
2016-08-03 23:46:18 +0300

    Product final version
    Conflicts:
    	src/main/java/lv/javaguru/java2/domain/Product.java

--- src/main/java/lv/javaguru/java2/domain/Product.java
+++ src/main/java/lv/javaguru/java2/domain/Product.java
@@ -1,4 +1,37 @@
 package lv.javaguru.java2.domain;
 
-public class Product {
+public class git commit Product {
+
+    private long id;
+    private long categoryID;
+
+    private String vendorCode;
+    private String vendorName;
+    private String vendorDescription;
+
+    private String unit;
+
+    private int price;
+
+    private String displayName;
+    private String displayDescription;
+
+    private int remainQty;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    @Override
+    public String toString() {
+        return "Product{" +
+                "productID=" + id +
+                ", categoryID=" + categoryID +
+                ", displayName='" + displayName + '\'' +
+                '}';
+    }
 }
