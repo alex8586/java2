151d2b26cb4d70c69d032f795372025fc5456e04
MaxJJ <mjefremov@gmail.com>
2016-08-06 01:42:11 +0300

    Product corrected

--- src/main/java/lv/javaguru/java2/domain/Product.java
+++ src/main/java/lv/javaguru/java2/domain/Product.java
@@ -26,6 +26,78 @@ public class Product {
         this.id = id;
     }
 
+    public long getCategoryID() {
+        return categoryID;
+    }
+
+    public void setCategoryID(long categoryID) {
+        this.categoryID = categoryID;
+    }
+
+    public String getVendorCode() {
+        return vendorCode;
+    }
+
+    public void setVendorCode(String vendorCode) {
+        this.vendorCode = vendorCode;
+    }
+
+    public String getVendorName() {
+        return vendorName;
+    }
+
+    public void setVendorName(String vendorName) {
+        this.vendorName = vendorName;
+    }
+
+    public String getVendorDescription() {
+        return vendorDescription;
+    }
+
+    public void setVendorDescription(String vendorDescription) {
+        this.vendorDescription = vendorDescription;
+    }
+
+    public String getUnit() {
+        return unit;
+    }
+
+    public void setUnit(String unit) {
+        this.unit = unit;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
+
+    public String getDisplayName() {
+        return displayName;
+    }
+
+    public void setDisplayName(String displayName) {
+        this.displayName = displayName;
+    }
+
+    public String getDisplayDescription() {
+        return displayDescription;
+    }
+
+    public void setDisplayDescription(String displayDescription) {
+        this.displayDescription = displayDescription;
+    }
+
+    public int getRemainQty() {
+        return remainQty;
+    }
+
+    public void setRemainQty(int remainQty) {
+        this.remainQty = remainQty;
+    }
+
     @Override
     public String toString() {
         return "Product{" +
