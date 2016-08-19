85ae7c6dc3438e754a8d72bafe32b0f4b9b3a2da
algpet <litovec@gmail.com>
2016-08-19 20:52:58 +0300

    -abstract parametrized test class for testing abstract dao -categoryDaoTest

--- src/test/java/lv/javaguru/java2/BaseEntityTest.java
+++ src/test/java/lv/javaguru/java2/BaseEntityTest.java
@@ -5,7 +5,11 @@ import lv.javaguru.java2.domain.BaseEntity;
 import org.junit.Before;
 import org.junit.Test;
 
-public abstract class BaseEntityTest<RecordClass extends BaseEntity, DAOClass extends DAOImpl> {
+import java.util.List;
+
+import static junit.framework.TestCase.*;
+
+public abstract class BaseEntityTest<RecordClass extends BaseEntity, DAOClass extends DAOImpl<RecordClass>> {
 
     protected RecordClass newRecord;
     protected RecordClass recordFromDAO;
@@ -17,20 +21,29 @@ public abstract class BaseEntityTest<RecordClass extends BaseEntity, DAOClass ex
     }
 
     protected abstract void initDAO();
-
     protected abstract RecordClass newRecord();
 
-    protected abstract void initRecordWithData(RecordClass record);
-    //abstract void makeChangesInRecord(RecordClass record1) throws Exception;
-    //abstract void compareRecords(RecordClass record1 , RecordClass record2);
+    protected abstract void fillRecordWithData(RecordClass record);
+
+    protected abstract void makeChangesInRecord(RecordClass record1);
+
+    protected abstract void compareRecords(RecordClass record1, RecordClass record2);
+
+    protected void insertRandomDummyRecords(int count) {
+        for (int i = 0; i < count; i++) {
+            RecordClass record = newRecord();
+            fillRecordWithData(record);
+            testedDAO.create(record);
+        }
+    }
 
     @Before
     public void setup() {
         cleaner.cleanDatabase();
         newRecord = newRecord();
-        initRecordWithData(newRecord);
+        fillRecordWithData(newRecord);
         testedDAO.create(newRecord);
-        recordFromDAO = (RecordClass) testedDAO.getById(newRecord.getId());
+        recordFromDAO = testedDAO.getById(newRecord.getId());
     }
 
     @Test(expected = IllegalArgumentException.class)
@@ -45,4 +58,88 @@ public abstract class BaseEntityTest<RecordClass extends BaseEntity, DAOClass ex
         testedDAO.create(fresh);
     }
 
+    @Test(expected = IllegalArgumentException.class)
+    public void updateWithNullFails() {
+        testedDAO.update(null);
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void updateWithZeroFails() {
+        RecordClass fresh = newRecord();
+        testedDAO.update(fresh);
+    }
+
+    @Test
+    public void deleteNullDoNothing() {
+        testedDAO.delete(null);
+    }
+
+    @Test
+    public void findWithWrongIdReturnsNull() {
+        RecordClass record = testedDAO.getById(-1);
+        assertNull(record);
+    }
+
+    @Test
+    public void createReturnsId() {
+        RecordClass record = newRecord();
+        fillRecordWithData(record);
+        testedDAO.create(record);
+        assertTrue(record.getId() > 0);
+    }
+
+    @Test
+    public void testCanFindCreatedRecord() {
+        assertTrue(recordFromDAO.getId() > 0);
+        compareRecords(newRecord, recordFromDAO);
+    }
+
+    @Test
+    public void testCouldNotFindDeletedRecord() {
+        long id = recordFromDAO.getId();
+        testedDAO.delete(recordFromDAO);
+        assertEquals(0, recordFromDAO.getId());
+        recordFromDAO = testedDAO.getById(id);
+        assertNull(recordFromDAO);
+    }
+
+    @Test
+    public void testCanSeeUpdatesAfterUpdate() {
+        makeChangesInRecord(recordFromDAO);
+        testedDAO.update(recordFromDAO);
+        RecordClass updatedRecord = testedDAO.getById(recordFromDAO.getId());
+        compareRecords(recordFromDAO, updatedRecord);
+    }
+
+    @Test
+    public void testGetAll() {
+        List<RecordClass> records;
+
+        records = testedDAO.getAll();
+        assertEquals(1, records.size());
+
+        insertRandomDummyRecords(3);
+        records = testedDAO.getAll();
+        assertEquals(4, records.size());
+
+        insertRandomDummyRecords(2);
+        records = testedDAO.getAll();
+        assertEquals(6, records.size());
+
+        testedDAO.delete(records.get(0));
+        records = testedDAO.getAll();
+        assertEquals(5, records.size());
+
+        testedDAO.delete(records.get(4));
+        records = testedDAO.getAll();
+        assertEquals(4, records.size());
+
+        testedDAO.delete(records.get(1));
+        records = testedDAO.getAll();
+        assertEquals(3, records.size());
+
+        insertRandomDummyRecords(5);
+        records = testedDAO.getAll();
+        assertEquals(8, records.size());
+    }
 }
--- src/test/java/lv/javaguru/java2/database/jdbc/CategoryDAOImplTest.java
+++ src/test/java/lv/javaguru/java2/database/jdbc/CategoryDAOImplTest.java
@@ -1,7 +1,13 @@
 package lv.javaguru.java2.database.jdbc;
 
 import lv.javaguru.java2.BaseEntityTest;
+import lv.javaguru.java2.database.DBException;
 import lv.javaguru.java2.domain.Category;
+import org.junit.Test;
+
+import java.util.Random;
+
+import static org.junit.Assert.assertEquals;
 
 public class CategoryDAOImplTest extends BaseEntityTest<Category, CategoryDAOImpl> {
 
@@ -16,144 +22,32 @@ public class CategoryDAOImplTest extends BaseEntityTest<Category, CategoryDAOImp
     }
 
     @Override
-    protected void initRecordWithData(Category category) {
-        category.setName("category");
+    protected void compareRecords(Category category1, Category category2) {
+        assertEquals(category1.getId(), category2.getId());
+        assertEquals(category1.getName(), category2.getName());
+    }
+
+    @Override
+    protected void fillRecordWithData(Category category) {
+        Random random = new Random();
+        category.setName("category" + random.nextInt(100000));
+    }
+
+    @Override
+    protected void makeChangesInRecord(Category category) {
+        category.setName("another " + category.getName());
     }
 
 
-
-    /*
-    private CategoryDAOImpl categoryDAOImpl = new CategoryDAOImpl();
-
-    @Test
-    public void findWithWrongIdReturnsNull() {
-        Category category = categoryDAOImpl.getById(-1);
-        assertNull(category);
-    }
-
-    @Test
-    public void testCreate() {
-        Category category = new Category("foo");
-        long id = categoryDAOImpl.create(category);
-        assertTrue(id > 0 );
-    }
-
-    @Test
-    public void canFindCreatedRecord() {
-        Category category = getDummyRecordFromDb();
-        long id = category.getId();
-        assertFalse(id == 0);
-        Category anotherCategory = categoryDAOImpl.getById(id);
-
-        assertNotNull(anotherCategory);
-        assertEquals(category.getName(), anotherCategory.getName());
-        assertEquals(category.getId(), anotherCategory.getId());
-    }
-
-    @Test(expected = IllegalArgumentException.class)
-    public void creatingCategoryWithIdShouldFail() {
-        Category category = new Category();
-        category.setName("Dairy");
-        long id1 = categoryDAOImpl.create(category);
-        assertTrue(true);
-        long id2 = categoryDAOImpl.create(category);
-    }
-
-    @Test(expected = IllegalArgumentException.class)
-    public void creatingCategoryWithNullShouldFail() {
-        Category category = null;
-        categoryDAOImpl.create(category);
-    }
-
-    @Test
-    public void testCanSeeUpdatesAfterUpdate() {
-        Category category = getDummyRecordFromDb();
-
-        long id = category.getId();
-        Category anotherCategory = categoryDAOImpl.getById(id);
-        anotherCategory.setName("another name");
-        categoryDAOImpl.update(anotherCategory);
-
-        Category yetAnotherCategory = categoryDAOImpl.getById(id);
-        assertEquals(anotherCategory.getName(),yetAnotherCategory.getName());
-        assertEquals(anotherCategory.getId(),yetAnotherCategory.getId());
-    }
-
-    @Test(expected = IllegalArgumentException.class)
-    public void updateNewRecordSholdFail() {
-        Category category = new Category();
-        category.setName("Dairy");
-        categoryDAOImpl.update(category);
-    }
     @Test(expected = DBException.class)
-    public void updateWithoutIdShouldFail() {
-        Category category = new Category();
-        category.setName("Dairy");
-        category.setId(-100500);
-        categoryDAOImpl.update(category);
-    }
-    @Test(expected = IllegalArgumentException.class)
-    public void updateNullRecordShouldFail() {
-        Category category = null;
-        categoryDAOImpl.update(category);
+    public void creatingWithNullNameFails() {
+        Category record = newRecord();
+        testedDAO.create(record);
     }
 
-    @Test
-    public void testCouldNotFindDeletedRecord() {
-        Category category = getDummyRecordFromDb();
-        long id = category.getId();
-
-        Category anotherCategory = categoryDAOImpl.getById(id);
-        categoryDAOImpl.delete(anotherCategory);
-        assertTrue(anotherCategory.getId() == 0);
-
-        Category yetAnotherCategory = categoryDAOImpl.getById(id);
-        assertNull(yetAnotherCategory);
+    @Test(expected = DBException.class)
+    public void updatingWithNullNameFails() {
+        recordFromDAO.setName(null);
+        testedDAO.update(recordFromDAO);
     }
-    @Test
-    public void testDelitingNullDoNothing() {
-        Category category = null;
-        categoryDAOImpl.delete(category);
-    }
-
-
-    @Test
-    public void testFindAll() {
-        insertDummyRecordToDb("name1");
-        insertDummyRecordToDb("name2");
-        insertDummyRecordToDb("name3");
-
-        List<Category> categories = categoryDAOImpl.getAll();
-        assertEquals(3 , categories.size());
-        assertEquals("name1" , categories.get(0).getName());
-        assertEquals("name2" , categories.get(1).getName());
-        assertEquals("name3" , categories.get(2).getName());
-
-        categoryDAOImpl.delete(categories.get(1));
-        categories = categoryDAOImpl.getAll();
-        assertEquals(2 , categories.size());
-        assertEquals("name1" , categories.get(0).getName());
-        assertEquals("name3" , categories.get(1).getName());
-
-        insertDummyRecordToDb("name4");
-        insertDummyRecordToDb("name5");
-
-        categories = categoryDAOImpl.getAll();
-        assertEquals(4 , categories.size());
-        assertEquals("name4" , categories.get(2).getName());
-        assertEquals("name5" , categories.get(3).getName());
-    }
-
-    private void insertDummyRecordToDb(String name) {
-        Category category = new Category();
-        category.setName(name);
-        categoryDAOImpl.create(category);
-    }
-
-    private Category getDummyRecordFromDb() {
-        Category category = new Category("name");
-        categoryDAOImpl.create(category);
-        return category;
-    }
-    */
 }
