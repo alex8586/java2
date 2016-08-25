631daff276732e06e14efd00f7c58c5341946e6e
ryujo <a.vitjukovs@gmail.com>
2016-08-25 15:44:41 +0300

    Fixed FP Controller test.

--- src/main/java/lv/javaguru/java2/database/ProductDAO.java
+++ src/main/java/lv/javaguru/java2/database/ProductDAO.java
@@ -2,9 +2,5 @@ package lv.javaguru.java2.database;
 
 import lv.javaguru.java2.domain.Product;
 
-/**
- * Created by algis on 16.25.8.
- */
 public interface ProductDAO extends CrudDAO<Product> {
-    Product getByVendorCode(String vendorCode);
 }
