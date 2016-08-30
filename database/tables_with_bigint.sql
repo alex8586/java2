d029521113993072a004e568b95db63e54916640
alex <mymail@somewhere.com>
2016-08-30 18:59:33 +0300

    added example file for creating tables with bigint

---
+++ database/tables_with_bigint.sql
@@ -0,0 +1,38 @@
+CREATE TABLE products (
+  id          BIGINT(11) NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  name        VARCHAR(255) NOT NULL,
+  description VARCHAR(500),
+  price       BIGINT NOT NULL DEFAULT 0,
+  imgurl      VARCHAR(100) NOT NULL
+);
+
+CREATE TABLE categories (
+  id        BIGINT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  name      VARCHAR(255) NOT NULL,
+  father_id BIGINT NOT NULL
+);
+
+ALTER TABLE products
+  ADD COLUMN category_id BIGINT(11);
+ALTER TABLE products
+  ADD FOREIGN KEY (category_id) REFERENCES categories (id)
+  ON DELETE SET NULL;
+
+CREATE TABLE users (
+  id       BIGINT(11)   NOT NULL PRIMARY KEY AUTO_INCREMENT,
+  name     VARCHAR(255) NOT NULL,
+  email    VARCHAR(255) NOT NULL,
+  password VARCHAR(255) NOT NULL,
+  UNIQUE (email)
+);
+
+CREATE TABLE shipping_profiles (
+  id       BIGINT(11)   NOT NULL PRIMARY KEY AUTO_INCREMENT,
+  person   VARCHAR(255) NOT NULL,
+  document VARCHAR(255) NOT NULL,
+  address  VARCHAR(255) NOT NULL,
+  phone    VARCHAR(50)  NOT NULL,
+  user_id  BIGINT(11),
+  CONSTRAINT FOREIGN KEY (user_id) REFERENCES users (id)
+    ON DELETE CASCADE
+);
