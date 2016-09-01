aaeac70270c26cdff65329a181b4bda487522ba2
algpet <litovec@gmail.com>
2016-09-01 20:24:36 +0300

    stock entity

---
+++ src/main/java/lv/javaguru/java2/domain/Stock.java
@@ -0,0 +1,97 @@
+package lv.javaguru.java2.domain;
+
+import org.apache.commons.lang3.builder.EqualsBuilder;
+import org.apache.commons.lang3.builder.HashCodeBuilder;
+
+import javax.persistence.Column;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import java.util.Date;
+
+public class Stock implements BaseEntity {
+    @Column(name = "id")
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long id;
+
+    @Column(name = "product_id")
+    private long productId;
+
+    @Column(name = "quantity")
+    private int quantity;
+
+    @Column(name = "expire_date")
+    private Date expireDate;
+
+    @Override
+    public long getId() {
+        return id;
+    }
+
+    @Override
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public long getProductId() {
+        return productId;
+    }
+
+    public void setProductId(long productId) {
+        this.productId = productId;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+
+    public Date getExpireDate() {
+        return expireDate;
+    }
+
+    public void setExpireDate(Date expireDate) {
+        this.expireDate = expireDate;
+    }
+
+    @Override
+    public boolean equals(final Object object) {
+        if (this == object) {
+            return true;
+        }
+        if ((object == null) || !(object instanceof Stock)) {
+            return false;
+        }
+        Stock that = (Stock) object;
+        return new EqualsBuilder()
+                .append(id, that.getId())
+                .append(productId, that.getProductId())
+                .append(quantity, that.getQuantity())
+                .append(expireDate, that.getExpireDate())
+                .isEquals();
+    }
+
+    @Override
+    public int hashCode() {
+        return new HashCodeBuilder()
+                .append(id)
+                .append(productId)
+                .append(quantity)
+                .append(expireDate)
+                .toHashCode();
+    }
+    
+    @Override
+    public String toString() {
+        return "Stock{" +
+                "id=" + id +
+                ", productId=" + productId +
+                ", quantity=" + quantity +
+                ", expireDate=" + expireDate +
+                '}';
+    }
+}
