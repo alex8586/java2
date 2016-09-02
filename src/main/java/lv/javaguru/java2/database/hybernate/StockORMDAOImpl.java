b86fc096e19959331775a40226bbf88c7f807cae
algpet <litovec@gmail.com>
2016-09-02 21:55:50 +0300

    stockOrmdaoimpl

--- src/main/java/lv/javaguru/java2/database/hybernate/StockORMDAOImpl.java
+++ src/main/java/lv/javaguru/java2/database/hybernate/StockORMDAOImpl.java
@@ -3,49 +3,84 @@ package lv.javaguru.java2.database.hybernate;
 import lv.javaguru.java2.database.StockDAO;
 import lv.javaguru.java2.domain.Product;
 import lv.javaguru.java2.domain.Stock;
+import lv.javaguru.java2.domain.User;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.criterion.Projections;
+import org.hibernate.criterion.Restrictions;
+import org.hibernate.criterion.SimpleExpression;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Scope;
+import org.springframework.context.annotation.ScopedProxyMode;
+import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.Date;
 import java.util.List;
 
-
+@Component("ORM_StockDAO")
+@Transactional
+@Scope(proxyMode = ScopedProxyMode.TARGET_CLASS)
 public class StockORMDAOImpl implements StockDAO {
+
+    @Autowired
+    SessionFactory sessionFactory;
+
     @Override
     public long create(Stock stock) {
-        return 0;
+        Session session = sessionFactory.getCurrentSession();
+        session.persist(stock);
+        session.flush();
+        return stock.getId();
     }
 
     @Override
     public void update(Stock stock) {
-
+        Session session = sessionFactory.getCurrentSession();
+        session.update(stock);
     }
 
     @Override
     public void delete(Stock stock) {
-
+        Session session = sessionFactory.getCurrentSession();
+        session.delete(stock);
     }
 
     @Override
     public Stock getById(long id) {
-        return null;
+        Session session = sessionFactory.getCurrentSession();
+        return (Stock) session.get(Stock.class, id);
     }
 
     @Override
     public List<Stock> getAll() {
-        return null;
+        Session session = sessionFactory.getCurrentSession();
+        return session.createCriteria(User.class).list();
     }
 
     @Override
     public List<Stock> allByProduct(Product product) {
-        return null;
+        Session session = sessionFactory.getCurrentSession();
+        return session.createCriteria(Stock.class).add(Restrictions.eq("product_id", product.getId())).list();
     }
 
     @Override
     public int countFreshByProduct(Product product, Date date) {
-        return 0;
+        SimpleExpression expression = Restrictions.ge("expireDate", date);
+        return countProductWithCriteria(product, expression);
     }
-
     @Override
     public int countExpiredByProduct(Product product, Date date) {
-        return 0;
+        SimpleExpression expression = Restrictions.le("expireDate", date);
+        return countProductWithCriteria(product, expression);
     }
+
+    private int countProductWithCriteria(Product product, SimpleExpression expression) {
+        Session session = sessionFactory.getCurrentSession();
+        return (int) session.createCriteria(Stock.class)
+                .add(Restrictions.eq("product_id", product.getId()))
+                .add(expression)
+                .setProjection(Projections.sum("count")).uniqueResult();
+    }
+
 }
