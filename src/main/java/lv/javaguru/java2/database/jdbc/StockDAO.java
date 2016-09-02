a8cf8dba8c4bd10f8b6b5eeb88c2356b8adf8618
algpet <litovec@gmail.com>
2016-09-02 21:15:52 +0300

    stock DAO interface

---
+++ src/main/java/lv/javaguru/java2/database/jdbc/StockDAO.java
@@ -0,0 +1,16 @@
+package lv.javaguru.java2.database.jdbc;
+
+import lv.javaguru.java2.database.CrudDAO;
+import lv.javaguru.java2.domain.Product;
+import lv.javaguru.java2.domain.Stock;
+
+import java.util.Date;
+import java.util.List;
+
+public interface StockDAO extends CrudDAO<Stock> {
+    List<Stock> allByProduct(Product product);
+
+    int countFreshByProduct(Product product, Date date);
+
+    int countExpiredByProduct(Product product, Date date);
+}
