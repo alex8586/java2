57fcf9f4f74a7fd014ec8a82111879f1fc87ed44
algpet <litovec@gmail.com>
2016-09-07 01:19:14 +0300

    pendingOrder class

---
+++ src/main/java/lv/javaguru/java2/businesslogic/PendingOrder.java
@@ -0,0 +1,12 @@
+package lv.javaguru.java2.businesslogic;
+
+import lv.javaguru.java2.domain.Cart;
+
+
+public interface PendingOrder {
+    Cart getCart();
+
+    void setCart(Cart cart);
+
+    boolean isPending();
+}
