ae789cbdfb26f6d3df84982dd2e7edf739da6f95
algpet <litovec@gmail.com>
2016-09-13 00:41:55 +0300

    requestedentity microdto

---
+++ src/main/java/lv/javaguru/java2/dto/RequestedEntity.java
@@ -0,0 +1,16 @@
+package lv.javaguru.java2.dto;
+
+/**
+ * Created by algis on 16.13.9.
+ */
+public class RequestedEntity {
+    private long id;
+
+    public RequestedEntity(long id) {
+        this.id = id;
+    }
+
+    public long getId() {
+        return id;
+    }
+}
---
+++ src/main/java/lv/javaguru/java2/dto/builders/RequestedEntityUtil.java
@@ -0,0 +1,17 @@
+package lv.javaguru.java2.dto.builders;
+
+import lv.javaguru.java2.dto.RequestedEntity;
+
+public class RequestedEntityUtil {
+    public RequestedEntity build(String requestId) {
+        if (requestId == null)
+            throw new NullPointerException();
+        long id = 0;
+        try {
+            id = Long.valueOf(requestId);
+        } catch (NumberFormatException e) {
+
+        }
+        return new RequestedEntity(id);
+    }
+}
